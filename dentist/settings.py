4e7c4bb4f81f1fb3ce28344d56f9872ccc39a3be
hrmtk <morgenzonne@zoho.com>
2021-08-03 16:59:41 +0900

    Added allowed hosts

--- dentist/settings.py
+++ dentist/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hrmtk-dentist.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
